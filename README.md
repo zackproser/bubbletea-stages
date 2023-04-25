9cae5c36f90b9ed6d5072da7c3bb81e758acd9da
Zachary Proser <zackproser@gmail.com>
2023-04-25 13:01:33 +0000

    Update code. Add README with demo gif

---
+++ README.md
@@ -0,0 +1,8 @@
+# Overview
+
+![Bubbbletea State Machine pattern](./stages.gif)
+
+This example repository demonstrates the Bubbletea State Machine pattern, which is ideal for building 
+command line tools and other Terminal User Interfaces (TUI) that can orchestrate complex deployments for your users.
+
+
--- main.go
+++ main.go
@@ -12,12 +12,18 @@ import (
 	"github.com/charmbracelet/lipgloss"
 )
 
+// Stage is a single step in a deployment process. Only one stage can be running at one time,
+// And the entire process exits if any stage fails along the way
+
+// The Action is the function that is run to complete the stage's work
+// IsComplete
 type Stage struct {
-	Name       string
-	Action     func() error
-	Error      error
-	IsComplete bool
-	Reset      func() error
+	Name           string
+	Action         func() error
+	Error          error
+	IsComplete     bool
+	IsCompleteFunc func() bool
+	Reset          func() error
 }
 
 var stageIndex = 0
@@ -27,23 +33,34 @@ var stages = []Stage{
 		Name: "One",
 		Action: func() error {
 			time.Sleep(3 * time.Second)
-			return errors.New("This one errored")
+			return nil
 		},
-		IsComplete: false,
+		IsCompleteFunc: func() bool { return false },
+		IsComplete:     false,
 	},
 	{
 		Name: "Two",
 		Action: func() error {
 			time.Sleep(3 * time.Second)
+			return errors.New("This one errored")
+		},
+		IsCompleteFunc: func() bool { return false },
+		IsComplete:     false,
+	},
+	{
+		Name: "Three",
+		Action: func() error {
+			time.Sleep(3 * time.Second)
 			return nil
 		},
-		IsComplete: false,
+		IsCompleteFunc: func() bool { return false },
+		IsComplete:     false,
 	},
 }
 
 type model struct {
 	status  int
-	err     error
+	Error   error
 	spinner spinner.Model
 }
 
@@ -54,9 +71,10 @@ func startDeployCmd() tea.Msg {
 }
 
 func runStage() tea.Msg {
-	currentStage := stages[stageIndex]
-	// Run the current stage, and record its result status
-	currentStage.Error = currentStage.Action()
+	if !stages[stageIndex].IsCompleteFunc() {
+		// Run the current stage, and record its result status
+		stages[stageIndex].Error = stages[stageIndex].Action()
+	}
 	return stageCompleteMsg{}
 }
 
@@ -84,18 +102,23 @@ func (m model) Init() tea.Cmd {
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
 	case stageCompleteMsg:
-		// Mark the current stage as complete and move to the next stage
+		// If we have an error, then set the error so that the views can properly update
+		if stages[stageIndex].Error != nil {
+			m.Error = stages[stageIndex].Error
+			writeCommandLogFile()
+			return m, tea.Quit
+		}
+		// Otherwise, mark the current stage as complete and move to the next stage
 		stages[stageIndex].IsComplete = true
-
+		// If we've reached the end of the defined stages, we're done
 		if stageIndex+1 >= len(stages) {
 			return m, tea.Quit
 		}
-
 		stageIndex++
 		return m, runStage
 
 	case errMsg:
-		m.err = msg
+		m.Error = msg
 		return m, tea.Quit
 
 	case tea.KeyMsg:
@@ -145,6 +168,42 @@ func (m model) View() string {
 		sb.WriteString(renderCheckbox(stage) + " " + renderWorkingStatus(m, stage) + "\n")
 	}
 	return sb.String()
+}
+
+// commandLog is rendered when the deployment encounters an error. It retains a log of all the "commands" that were run in the course of deploying the example
+// "commands" are intentionally in air-quotes here because this also includes things like checking for the existence of environment variables, and is not yet
+// implemented in a truly re-windable cross-platform way, but it's a start, and it's better than asking someone over an email what failed
+var commandLog = []string{}
+
+func logCommand(s string) {
+	commandLog = append(commandLog, s)
+}
+
+func writeCommandLogFile() {
+	//Write the entire command log to a file on the filesystem so that the user has the option of sending it to Gruntwork for debugging purposes
+	// We currently write the file to ./gruntwork-examples-debug.log in the same directory as the executable was run in
+
+	// Create the file
+	f, err := os.Create("bubbletea-debug.log")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	// Write to the file, first writing the UTC timestamp as the first line, then looping through the command log to write each command on a new line
+	f.WriteString("Ran at: " + time.Now().UTC().String() + "\n")
+	f.WriteString("******************************************************************************\n")
+	f.WriteString("Human legible log of steps taken and commands run up to the point of failure:\n")
+	f.WriteString("******************************************************************************\n")
+	for _, cmd := range commandLog {
+		f.WriteString(cmd + "\n")
+	}
+	f.WriteString("^ The above command is likely the one that caused the error!\n")
+	f.WriteString("\n\n")
+	f.WriteString("******************************************************************************\n")
+	f.WriteString("Complete log of the error that halted the deployment:\n")
+	f.WriteString("******************************************************************************\n")
+	f.WriteString("\n\n")
+	f.WriteString(stages[stageIndex].Error.Error() + "\n")
 }
 
 func main() {
